bac04691e30d6519c48a050959e8303050d30835
unipota <woz.eon@gmail.com>
2019-03-29 20:46:37 +0900

    hello webgl

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, './src/main.ts'), 
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader'
+      },
+      {
+        test: /\.html$/
+      },
+      {
+        test: /\.(frag|vert|glsl)$/,
+        use: 'raw-loader',
+      }
+    ]
+  },
+  devServer: {
+    watchContentBase: true,
+    contentBase: path.resolve(__dirname, './public'),
+    inline: true,
+    open: false,
+    port: 1234
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  resolve: {
+    extensions: [
+      '.ts', '.js'
+    ]
+  }
+};
